c330b6d1940bafa4eed26a2c6d9dce895b41301c
aleks <aleks.valushko@yandex.ru>
2020-09-13 00:03:28 +0300

    refactored

--- src/component/Footer/MenuList/Catalog/Catalog.jsx
+++ src/component/Footer/MenuList/Catalog/Catalog.jsx
@@ -11,9 +11,7 @@ const Catalog = (props) => {
             <div className={mod.title}>КАТАЛОГ</div>
             <ul className={mod.list}>
                 {
-                    catalog.map( (el, index) => <NavLink to={el} key={index}>
-                        <li>{el}</li>
-                    </NavLink>)
+                    catalog.map( (el, index) => <NavLink to={el} key={index}>{el}</NavLink>)
                 }
             </ul>
         </div>
